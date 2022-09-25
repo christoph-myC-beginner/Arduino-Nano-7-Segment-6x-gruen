a3b4f0cd061da028ba49a29cc4f154e3570bd2c9
christoph-myC-beginner <christoph.schumachers@gmx.de>
2022-09-25 19:17:57 +0200

    testing again

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,39 @@ int latchPin = 7;  // RCLK (Register Clock / Latch) Pin des 74HC595 ist verbunde
 int clockPin = 8; // SRCLK (Shit Register Clock) Pin des 74HC595 ist verbunden mit dem digitalen Pin 6
 int dataPin = 5;  // SER (Serial input) Pin des 74HC595 ist verbunden mit dem digitalen Pin 4
 int enablePin = 6;
-
+//                    a,b,c,d,e,f,g,p,0,0,6,5,4,3,2,1
 int testArr[8][16]= {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                      {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1},
-                     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-                     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
-                     {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0}};
+                     {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                     {0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+                     {0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+                     {0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0},
+                     {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+                     {1,1,1,1,1,1,0,1,0,0,1,0,0,0,0,0}};
 
-int delTime = 200;
+int targetArr[8][16];
+
+int digitArr[7][8]={{0,0,0,0,0,0,0,0}, //keine Stelle
+                    {0,0,0,0,0,0,0,1}, //Stelle 1 von rechts
+                    {0,0,0,0,0,0,1,0}, //Stelle 2 von rechts
+                    {0,0,0,0,0,1,0,0}, //Stelle 3 von rechts
+                    {0,0,0,0,1,0,0,0}, //Stelle 4 von rechts
+                    {0,0,0,1,0,0,0,0}, //Stelle 5 von rechts
+                    {0,0,1,0,0,0,0,0}};//Stelle 6 von rechts
+
+int numArr[11][8]={{0,0,0,0,0,0,0,0}, //keine Ziffer
+                   {1,0,0,1,1,1,1,1}, //Ziffer 1
+                   {0,0,1,0,0,1,0,1},
+                   {0,0,0,0,0,0,0,1},
+                   {0,0,0,0,0,0,0,1},
+                   {0,0,0,0,0,0,0,1},
+                   {0,0,0,0,0,0,0,1},
+                   {0,0,0,0,0,0,0,1},
+                   {0,0,0,0,0,0,0,1},
+                   {0,0,0,0,0,0,0,1}
+                   {0,0,0,0,0,0,0,1}} //
+
+int delTime = 800;
 
 //declare functions (functions are written below)
 void putShiftRegister(int x);
@@ -33,6 +55,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i<8; i++) {
+    //targetArr[i,j]=
     putShiftRegister(i); 
     delay(delTime);
   }
